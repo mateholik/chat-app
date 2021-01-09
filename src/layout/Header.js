f445abfa38ed0a1c4587d0c0a410b67096e22062
mateholik <vaisov.vladimir@gmail.com>
2021-01-10 00:28:50 +0200

    form, validations

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,9 +1,48 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header className="mb-4">
+      <nav className="navbar navbar-dark bg-primary">
+        <div className="container">
+          <NavLink className="navbar-brand" to="/">
+            Chat app
+          </NavLink>
+          <ul className="nav">
+            <li className="nav-item">
+              <NavLink
+                exact
+                className="nav-link"
+                to="/login"
+                activeClassName="active"
+              >
+                Login
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                exact
+                className="nav-link"
+                to="/profile"
+                activeClassName="active"
+              >
+                Profile
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                exact
+                className="nav-link"
+                to="/"
+                activeClassName="active"
+              >
+                Chat
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
   );
 }
--- src/views/Login.js
+++ src/views/Login.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Form from "./../components/LoginForm";
 
 export default function Login() {
   return (
     <div>
       <h1>Login</h1>
+      <Form></Form>
     </div>
   );
 }
